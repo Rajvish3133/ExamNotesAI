2a2afef62cd1b7a96604c8ab1cf5967ee4a0a099
Raj Vishwakarma <rv2477207@gmail.com>
2026-08-19 16:29:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://examnotesaiclient-6zga.onrender.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
